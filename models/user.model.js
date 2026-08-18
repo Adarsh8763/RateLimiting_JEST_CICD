4bb6789c2615603b40b8201ffa83ac9cd4ffdd5b
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-08-18 08:12:25 +0530

    Restore required field for email in user schema

--- models/user.model.js
+++ models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         unique: true,
-        // required: true
+        required: true
     },
     password: String
 })
